a0ffbeb862210bd1b072b95351e3c3637751451d
Joseph Mbai <31988614+m0ahs@users.noreply.github.com>
2025-09-14 00:02:16 +0200

    Initial Commit

---
+++ Mel/MelApp.swift
@@ -0,0 +1,17 @@
+//
+//  MelApp.swift
+//  Mel
+//
+//  Created by Joseph Mbai Bisso on 14.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct MelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
